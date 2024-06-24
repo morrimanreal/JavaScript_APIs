e6c393f852907252b266f7ea668bf0b682728a3e
Morricone Manreal <morriconemanreal@gmail.com>
2024-06-24 18:14:16 -0400

    added notes for future references

--- app.js
+++ app.js
@@ -15,10 +15,13 @@ let para = document.createElement("p");
 // para.textContent = "paragraph";
 // document.body.append(para);
 
-//fetch the API based on users input when the user clicks the button
+
+//when the user clicks the button, fetch the API, then search the unpackaged object from the API and show it to the browser
 searchButton.addEventListener("click", () =>{
+  //fetch the API 
   fetch(`https://api.giphy.com/v1/gifs/translate?apiKey=MaZofcmPTA3qPriwyIz4fAer2CxwRS0K&s=${searchInput.value}`)
 
+  //unpackage the returned API information to be used 
 .then((response) => {
   console.log(response)
   return response.json();
@@ -40,3 +43,4 @@ searchButton.addEventListener("click", () =>{
 })
 })
 
+//everytime .then returns a promise as its value, will have to keep adding .then, but once it stops returning a promise value.  .catch is the end to show errors.
